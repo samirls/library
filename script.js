01053e900fa19455f258e6f4b7c4210efa87418e
samirls <samirlaguardiaii@gmail.com>
2022-10-26 15:59:21 -0300

    javascript halfway done

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ function clouds() {
 function day() {
     document.body.style.backgroundImage = "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)";
     document.body.style.color = "black";
+    document.body.style.textShadow = "none";
     document.getElementById("dayNight").src = "./img/weather-night.png";
     document.getElementById("addBook").src = "./img/add.png";
     isDay = 0;
@@ -57,7 +58,7 @@ function changeColor() {
 
     if (isDay === 1) {
         return clouds();
-    }
+    } 
 
     if (isDay === 2) {
         return day();
@@ -66,10 +67,94 @@ function changeColor() {
 }
 
 
-//Code for library
+//<-------------> Code for library Starts Here <--------------> stop 27:15
 
-//Library
-let myLibrary = [];
+//books - main div holding all the books
+const books = document.querySelector('.books');
+
+//Library (array of books)
+let myLibrary = [{
+    title: "Example",
+    author: "me",
+    pages: 500,
+    read: true
+}, {
+    title: "Example 2",
+    author: "you",
+    pages: 100,
+    read: false,
+}
+];
+
+//Create elements for each book (helper function to create HTML elements
+//with text content and classes)
+function createBookElement(el, content, className) {
+    const element = document.createElement(el);
+    element.textContent = content;
+    element.setAttribute("class", className);
+    return element;
+}
+
+//read or not function (helper function to create input checkbox for if
+//book is read w/ event listener
+
+function createReadElement(bookItem, book) {
+    let read = document.createElement("span");
+    read.setAttribute("class", "book-read");
+    read.appendChild(createBookElement("h1", "Read?", "book-read-title"));
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.addEventListener("click", (e)=> {
+        if(e.target.checked) {
+            bookItem.setAttribute("class", "card book read-checked")
+            book.read = true;
+            renderBooks()
+        }
+        else {
+            bookItem.setAttribute("class", "card book read-unchecked");
+            book.read = false;
+            renderBooks();
+        }
+    })
+
+    if (book.read){
+        input.checked = true;
+        bookItem.setAttribute("class", "card book read-checked");
+    }
+    read.appendChild(input);
+    return read;
+
+}
+
+//create the edit icon w/ event listener
+function createEditIcon(book) {
+    const editIcon = document.createElement("img");
+    editIcon.src = "./img/pencil-white.png";
+    editIcon.setAttribute("class", "edit-icon");
+    editIcon.addEventListener("click", (e) => {
+        console.log(book);
+    });
+    return editIcon;
+}
+
+
+
+//create divs for each new book (function to create all of the book content
+// on the book dom card)
+function createBookItem (book, index) {
+    const bookItem = document.createElement("div");
+    bookItem.setAttribute("id", index);
+    bookItem.setAttribute("key", index);
+    bookItem.setAttribute("class", "card book");
+    bookItem.appendChild(createBookElement('h1', `Title: ${book.title}`, "book-title"));
+    bookItem.appendChild(createBookElement('h1', `Author: ${book.author}`, "book-author"));
+    bookItem.appendChild(createBookElement('h1', `Pages: ${book.pages}`, "book-pages"));
+    bookItem.appendChild(createReadElement(bookItem, book));
+    bookItem.appendChild(createBookElement("button", "X", "delete"));
+    bookItem.appendChild(createEditIcon(book));
+    books.insertAdjacentElement("afterbegin", bookItem);
+
+}
 
 //book constructor
 function Book(title, author, pages, read) {
@@ -81,3 +166,14 @@ function Book(title, author, pages, read) {
         return (title + author + pages + read);
     }
 }
+
+//render books (function to render all the books)
+function renderBooks() {
+    myLibrary.map((book,index) => {
+        createBookItem(book,index)
+    })
+}
+
+
+//render on page load
+renderBooks();
